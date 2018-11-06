0ade07cfd98147594868dd3830b39328167283b5
Jadsada <59010237@kmitl.ac.th>
2018-11-06 16:52:31 +0700

    can get info and logout

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -29,6 +29,18 @@ export class HomeComponent implements OnInit {
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
   }
 
+  checkInfo(googleUser) {
+    var auth2 = gapi.auth2.getAuthInstance();
+    if (auth2.isSignedIn.get()) {
+      var profile = auth2.currentUser.get().getBasicProfile();
+      console.log('ID: ' + profile.getId());
+      console.log('Full Name: ' + profile.getName());
+      console.log('Given Name: ' + profile.getGivenName());
+      console.log('Family Name: ' + profile.getFamilyName());
+      console.log('Image URL: ' + profile.getImageUrl());
+      console.log('Email: ' + profile.getEmail());
+    }
+      }
   signOut() {  
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
