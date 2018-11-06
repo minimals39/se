037461af1acd1790603b1e24ad2be6f5c92f5d12
Jadsada <59010237@kmitl.ac.th>
2018-11-06 17:29:48 +0700

    login post email to backend

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -27,8 +27,16 @@ export class HomeComponent implements OnInit {
     var profile = googleUser.getBasicProfile();
     var email = profile.getEmail();
     if( email.indexOf('kmitl.ac.th') <= 0){
-    console.log('log in with kmitl email you idiot')
+          document.getElementById("status").innerHTML = 'log in with kmitl email you idiot';
     this.signOut()    }
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://yourbackend.example.com/tokensignin');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onload = function() {
+      console.log('Signed in as: ' + xhr.responseText);
+    };
+    xhr.send('idtoken=' + email);
+
   }
 
   checkInfo(googleUser) {
@@ -42,23 +50,30 @@ export class HomeComponent implements OnInit {
       console.log('Family Name: ' + profile.getFamilyName());
       console.log('Image URL: ' + profile.getImageUrl());
       console.log('Email: ' + profile.getEmail());
+      
     }
+    else{
+      document.getElementById("status").innerHTML = 'not logged in';
+    }
+
       }
   checkName(googleUser) {
     var auth2 = gapi.auth2.getAuthInstance();
     if (auth2.isSignedIn.get()) {
       var profile = auth2.currentUser.get().getBasicProfile();
       this.name = profile.getName();
-      console.log('Name: ' + this.name);
-    }
+        document.getElementById("status").innerHTML = 'Name: ' +this.name;
+    
+          }
     else{
-      console.log('not logged in');
+      document.getElementById("status").innerHTML = 'not logged in';
     }
       }
   signOut() {  
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
-      console.log('User signed out.');
+      document.getElementById("status").innerHTML = 'User signed out.';
     });
   }
+  
 }
