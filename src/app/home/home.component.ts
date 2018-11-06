19400e251bef86137c6e613975ba218abf037e03
Jadsada <59010237@kmitl.ac.th>
2018-11-06 17:02:41 +0700

    filter non kmitl

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,6 +9,8 @@ import { DataService } from '../data.service';
 })
 export class HomeComponent implements OnInit {
   auth2: any;
+  name: any;
+
 
   constructor(private data: DataService) { }
 
@@ -23,22 +25,34 @@ export class HomeComponent implements OnInit {
  
   onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    var email = profile.getEmail();
+    if( email.indexOf('kmitl.ac.th') <= 0){
+    console.log('log in with kmitl email you idiot')
+    this.signOut()    }
   }
 
   checkInfo(googleUser) {
     var auth2 = gapi.auth2.getAuthInstance();
     if (auth2.isSignedIn.get()) {
       var profile = auth2.currentUser.get().getBasicProfile();
+      this.name = profile.getName();
       console.log('ID: ' + profile.getId());
       console.log('Full Name: ' + profile.getName());
       console.log('Given Name: ' + profile.getGivenName());
       console.log('Family Name: ' + profile.getFamilyName());
       console.log('Image URL: ' + profile.getImageUrl());
       console.log('Email: ' + profile.getEmail());
+    }
+      }
+  checkName(googleUser) {
+    var auth2 = gapi.auth2.getAuthInstance();
+    if (auth2.isSignedIn.get()) {
+      var profile = auth2.currentUser.get().getBasicProfile();
+      this.name = profile.getName();
+      console.log('Name: ' + this.name);
+    }
+    else{
+      console.log('not logged in');
     }
       }
   signOut() {  
